43d0c1f0d7fae7ff69735488002d10464d871c8f
jamiladabachine <j.dabachine3378@uca.ac.ma>
2025-11-17 10:56:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fragmentslab/FragmentOne.java
@@ -0,0 +1,30 @@
+package com.example.fragmentslab;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FragmentOne extends Fragment {
+
+    private TextView tv;
+    private Button btnHello;
+
+    public FragmentOne() {
+        super(R.layout.fragment_one);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        tv = view.findViewById(R.id.textOne);
+        btnHello = view.findViewById(R.id.btnHello);
+
+        btnHello.setOnClickListener(v -> tv.setText("Bonjour depuis Fragment 1 !"));
+    }
+}
